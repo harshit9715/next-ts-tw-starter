42217ef96f227b90756d50d2ac72f2ad9e4e6052
harshit9715 <harshit9715@gmail.com>
2022-07-14 14:26:46 +0530

    feat: add BaseTemplate component

---
+++ components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+export default {
+  title: 'Templates/BaseTemplate',
+  component: BaseTemplate,
+  argTypes: {
+    backgroundColor: {},
+  },
+} as ComponentMeta<typeof BaseTemplate>;
+
+const Template: ComponentStory<typeof BaseTemplate> = (args) => (
+  <BaseTemplate {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockBaseTemplateProps.base,
+} as IBaseTemplate;
