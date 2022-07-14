08e1926acb820305cb36d0a034fc7ee1e38a4e55
harshit9715 <harshit9715@gmail.com>
2022-07-14 15:55:16 +0530

    feat: cat card component

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,11 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  author: 'Alex',
+  body: 'Lorem ipsum is the name of the cat here or there I dont know what to call it',
+  tag: 'Felines',
+  time: '2h ago',
+  title: 'Whats new in the cat world',
+};
+
+export const mockCatCardProps = { base };
